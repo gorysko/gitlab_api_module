7ec374f961b186c2ed22d1f7ed48e13fb59ba1af
Igor.Lushchyk <ihor.lushchyk@sethq.com>
2015-01-13 10:47:08 +0200

    cleanedup and fixed

--- gitlab.py
+++ gitlab.py
@@ -26,72 +26,72 @@ class GitlabApi(object):
 
         post_query = urlencode(query)
         response = urlopen(url + post_query)
-        return _return_result(response)
+        return self._return_result(response)
 
 
     def get_project(self, project_id):
         """Gets project by project id"""
-        project_id = _check_type(project_id)
+        project_id = self._check_type(project_id)
 
         url = self._url + 'projects/' + project_id + '?'
         query = {'private_token': self._private_token}
         post_query = urlencode(query)
 
         response = urlopen(url + post_query)
-        return _return_result(response)
+        return self._return_result(response)
 
 
     def get_events(self, project_id):
         """Get project events by project_id"""
-        project_id = _check_type(project_id)
-        url = URL + 'projects/' + project_id + '/' + 'events?'
+        project_id = self._check_type(project_id)
+        url = self._url + 'projects/' + project_id + '/' + 'events?'
         query = {'private_token': self._private_token}
         post_query = urlencode(query)
         response = urlopen(url + post_query)
-        return _return_result(response)
+        return self._return_result(response)
 
 
     def get_members(self, project_id):
         """gets project members by project_id"""
-        project_id = _check_type(project_id)
+        project_id = self._check_type(project_id)
         url = self._url + 'projects/' + project_id + '/' + 'members?'
         query = {'private_token': self._private_token}
         post_query = urlencode(query)
         response = urlopen(url + post_query)
-        return _return_result(response)
+        return self._return_result(response)
 
     def users(self):
         """Gets list of users"""
         response = urlopen(self._url + 'users?' + self._private_token)
-        return _return_result(response)
+        return self._return_result(response)
 
 
     def get_user(self, user_id):
         """Gets user by it's id"""
-        user_id = _check_type(user_id)
+        user_id = self._check_type(user_id)
         response = urlopen(self._url + 'users/' + user_id + '?' + \
                            self._private_token)
-        return _return_result(response)
+        return self._return_result(response)
 
 
     def get_user_keys(self, user_id):
         """Gets user ssh keys"""
-        user_id = _check_type(user_id)
+        user_id = self._check_type(user_id)
         response = urlopen(self._url + 'users/' + user_id + '/keys' + '?' + \
                            self._private_token)
-        return _return_result(response)
+        return self._return_result(response)
 
-
-def _return_result(response):
-    """Reads and checks response"""
-    result = response.read()
-    if result is not None:
-        return result
+    @staticmethod
+    def _return_result(response):
+        """Reads and checks response"""
+        if response is not None:
+            return  response.read()
         return None
 
 
-def _check_type(item):
-    """Checks and casts type of item"""
-    if type(item) == int:
-        return str(item)
+    @staticmethod
+    def _check_type(item):
+        """Checks and casts type of item"""
+        if type(item) == int:
+            return str(item)
         return item
