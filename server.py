dd40541b3687eaa566a105b5ea8ddf83b1dc96f6
Igor.Lushchyk <gorysko@gmail.com>
2015-01-22 15:12:08 +0200

    a bit changed

--- server.py
+++ server.py
@@ -3,12 +3,20 @@
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world(name=None):
     return render_template('first.html', name=name)
+
+
+@app.route('/stats', methods=['GET', 'POST'])
+def get():
+    user = request.args.get('user', '')
+    repo = request.args.get('repo_name', '')
+    return render_template('stats.html', user=user, repo=repo)
 
 if __name__ == '__main__':
     app.run(debug=True)
