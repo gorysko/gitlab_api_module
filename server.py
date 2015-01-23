7677e99f57142de67f88ab53362b1a8149e7b471
Igor.Lushchyk <gorysko@gmail.com>
2015-01-23 15:16:29 +0200

    a bt fixed ui

--- server.py
+++ server.py
@@ -18,10 +18,11 @@ def hello_world(name=None):
 def get():
     user = request.args.get('user', '')
     repo = request.args.get('repo_name', '')
-    info = request.args.get('info', '')
     api = _init_api(user)
     return render_template('stats.html', user=user, repo=repo,
-                           result=api.get_repo_info(repo, info))
+                           languages=api.get_repo_info(repo, 'languages'),
+                           branches=api.get_repo_info(repo, 'branches'),
+                           contributors=api.get_repo_info(repo, 'contributors'))
 
 
 def _init_api(user_name, git=True):
