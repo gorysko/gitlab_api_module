d74ebb1b95ed9e2d28e52eca0eddaf55a6aa3160
Igor Lushchyk <ihor.lushchyk@sethq.com>
2015-02-13 16:15:36 +0200

    fixed auth

--- server.py
+++ server.py
@@ -6,9 +6,12 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import g
+from flask import url_for
+from flask import jsonify
 from flask import redirect
 from flask.ext.github import GitHub
-from application import github
+
+from application import github as git_wapper
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column
@@ -19,24 +22,25 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URI = 'sqlite:////tmp/github.db'
 SECRET_KEY = 'development key'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config['GITHUB_CLIENT_ID'] = 'XXX'
-app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
+app.config['DATABASE_URI'] = 'sqlite:////tmp/github.db'
 
-app.config['GITHUB_BASE_URL'] = 'https://HOSTNAME/api/v3/'
-app.config['GITHUB_AUTH_URL'] = 'https://HOSTNAME/login/oauth/'
+app.config['GITHUB_CLIENT_ID'] = '5a80a178d27e64a4d264'
+app.config['GITHUB_CLIENT_SECRET'] = '243aa848374960e115494977cada492466f47902'
+
+app.config['GITHUB_BASE_URL'] = 'https://api.github.com/'
+app.config['GITHUB_AUTH_URL'] = 'https://github.com/login/oauth/'
 
 github = GitHub(app)
 
 engine = create_engine(app.config['DATABASE_URI'])
 db_session = scoped_session(sessionmaker(autocommit=False,
-autoflush=False,
-bind=engine))
+                                         autoflush=False,
+                                         bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
@@ -49,7 +53,7 @@ def init_db():
 class User(Base):
     """User class."""
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     username = Column(String(200))
     github_access_token = Column(String(200))
@@ -59,9 +63,11 @@ class User(Base):
 
 @app.before_request
 def before_request():
-    g.user = None
+    g.user_id = None
+    g.user_metada = None
     if 'user_id' in session:
-        g.user = User.query.get(session['user_id'])
+        g.user_id = User.query.get(session['user_id'])
+        g.user_metada = github.get('user')
 
 @app.after_request
 def after_request(response):
@@ -70,12 +76,12 @@ def after_request(response):
 
 @app.route('/', methods=['GET'])
 def index(name=None):
-    return render_template('first.html', name=name, user=g.user)
+    return render_template('first.html', name=name, user=g.user_metada)
 
 
 @github.access_token_getter
 def token_getter():
-    user = g.user
+    user = g.user_id
     if user is not None:
         return user.github_access_token
 
@@ -97,7 +103,6 @@ def authorized(access_token):
     next_url = request.args.get('next') or url_for('index')
     if access_token is None:
         return redirect(next_url)
-
     user = User.query.filter_by(github_access_token=access_token).first()
     if user is None:
         user = User(access_token)
@@ -125,20 +130,17 @@ def logout():
 
 @app.route('/user')
 def user():
-    return str(github.get('user'))
-
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    return str(g.user_metada)
 
 def _init_api(user_name, api_url, git=True):
     if git:
         if api_url is None:
-            api_url = 'https://api.github.com/'
-        api = github.GithubApi(api_url, user_name)
+            api_url = app.config['GITHUB_BASE_URL']
+        api = git_wapper.GithubApi(api_url, user_name)
         return api
     return None
 
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
